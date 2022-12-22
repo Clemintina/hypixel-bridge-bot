b3eed9dd685639f8f2113a010a4177a5deb8cae7
Clemintina <git@seraph.si>
2022-12-22 16:02:11 +0000

    refactor: cleaned up BedwarsStatisticsCommand to be cleaner

--- src/commands/BedwarsStatisticsCommand.ts
+++ src/commands/BedwarsStatisticsCommand.ts
@@ -13,35 +13,20 @@ class BedwarsStatisticsCommand extends CommandBase {
         try {
             const hypixelClient = new Client(getAppConfig().hypixelApiKey);
             const cleanPlayerName = sanatiseMessage(player).trim();
-            if (params.length == 0) {
-                const playerUuid = await getPlayerUuid(cleanPlayerName);
-                const playerStats = await hypixelClient.player.uuid(playerUuid);
-                if (playerStats) {
-                    const bedwars = playerStats.stats.Bedwars;
-                    const star = playerStats?.achievements?.bedwars_level ?? 0;
-                    const fkdr = formatRatio(bedwars?.final_kills_bedwars ?? 0, bedwars?.final_deaths_bedwars ?? 0);
-                    const bblr = formatRatio(bedwars?.beds_broken_bedwars ?? 0, bedwars?.beds_lost_bedwars ?? 0);
-                    const wlr = formatRatio(bedwars?.wins_bedwars ?? 0, bedwars?.losses_bedwars ?? 0);
-                    const finals = bedwars?.final_kills_bedwars;
-                    this.getBotInstance().chat(`/gc ${star} ${getPlayerRank(playerStats).cleanPrefix} ${playerStats.displayname} FKDR: ${fkdr} BBLR: ${bblr} WLR: ${wlr} FK: ${finals}`);
-                } else {
-                    this.getBotInstance().chat(`/gc The player ${cleanPlayerName} is invalid!`);
-                }
+
+            const playerUuid = await getPlayerUuid(params.length == 0 ? cleanPlayerName : params[0].trim());
+            const playerStats = await hypixelClient.player.uuid(playerUuid);
+
+            if (playerStats) {
+                const bedwars = playerStats.stats.Bedwars;
+                const star = playerStats?.achievements?.bedwars_level ?? 0;
+                const fkdr = formatRatio(bedwars?.final_kills_bedwars ?? 0, bedwars?.final_deaths_bedwars ?? 0);
+                const bblr = formatRatio(bedwars?.beds_broken_bedwars ?? 0, bedwars?.beds_lost_bedwars ?? 0);
+                const wlr = formatRatio(bedwars?.wins_bedwars ?? 0, bedwars?.losses_bedwars ?? 0);
+                const finals = bedwars?.final_kills_bedwars;
+                this.getBotInstance().chat(`/gc ${star} ${getPlayerRank(playerStats).cleanPrefix} ${playerStats.displayname} FKDR: ${fkdr} BBLR: ${bblr} WLR: ${wlr} FK: ${finals}`);
             } else {
-                const cleanParamPlayerName = params[0].trim();
-                const playerUuid = await getPlayerUuid(cleanParamPlayerName);
-                const playerStats = await hypixelClient.player.uuid(playerUuid);
-                if (playerStats) {
-                    const bedwars = playerStats.stats.Bedwars;
-                    const star = playerStats?.achievements?.bedwars_level ?? 0;
-                    const fkdr = formatRatio(bedwars?.final_kills_bedwars ?? 0, bedwars?.final_deaths_bedwars ?? 0);
-                    const bblr = formatRatio(bedwars?.beds_broken_bedwars ?? 0, bedwars?.beds_lost_bedwars ?? 0);
-                    const wlr = formatRatio(bedwars?.wins_bedwars ?? 0, bedwars?.losses_bedwars ?? 0);
-                    const finals = bedwars?.final_kills_bedwars;
-                    this.getBotInstance().chat(`/gc ${star} ${getPlayerRank(playerStats).cleanPrefix} ${playerStats.displayname} FKDR: ${fkdr} BBLR: ${bblr} WLR: ${wlr} FK: ${finals}`);
-                } else {
-                    this.getBotInstance().chat(`/gc The player ${cleanPlayerName} is invalid!`);
-                }
+                this.getBotInstance().chat(`/gc The player ${cleanPlayerName} is invalid!`);
             }
         } catch (e) {
             console.log(e);
