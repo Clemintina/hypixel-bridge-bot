d7329e255d9adcdcb73fbe091aeca592896ded09
kk <kk>
2023-02-09 16:18:34 +0000

    feat: guild requirements are an embed and customisable in the config file

--- src/discord/GuildRequirements.ts
+++ src/discord/GuildRequirements.ts
@@ -1,13 +1,14 @@
-import { ChatInputCommandInteraction, Client } from "discord.js";
+import { ChatInputCommandInteraction, Client, EmbedBuilder } from "discord.js";
 import { ConfigFile } from "../util/CustomTypes";
 import { SeraphCache } from "../util/SeraphCache";
 import { formatRatio } from "../util/CommonUtils";
+import { Color } from "chalk";
 
 const GuildRequirements = async (client: Client, interaction: ChatInputCommandInteraction): Promise<void> => {
 	if (interaction.commandName == "reqcheck") {
 		const config = require("../../config.json5") as ConfigFile;
 
-		let playerData: { uuid: string; name: string; gxpTotal: number;};
+		let playerData: { uuid: string; name: string; gxpTotal: number };
 		const hypixelClient = new SeraphCache();
 
 		const playername = interaction.options.getString("name") as string;
@@ -26,18 +27,38 @@ const GuildRequirements = async (client: Client, interaction: ChatInputCommandIn
 						playerData = { ...playerData, gxpTotal };
 					}
 				}
-
-				const bedwars = hypixelPlayer?.stats?.Bedwars;
-				const duels = hypixelPlayer?.stats?.Duels;
-
-				const duelsWins = duels?.[`wins`] ? (duels[`wins`] as number) : 0;
-				const duelsLosses = duels?.[`losses`] ? (duels[`losses`] as number) : 0;
-				const duelsWLR = formatRatio(duelsWins, duelsLosses);
-
-				const message = `Bedwars:\nLevel: ${hypixelPlayer?.achievements?.bedwars_level ?? 0}\n Wins: ${bedwars?.wins_bedwars ?? 0}\n\nDuels:\nWins: ${duelsWins}\nWLR: ${duelsWLR}`;
-
-				await interaction.editReply(message);
 			}
+
+			const bedwars = hypixelPlayer?.stats?.Bedwars;
+			const duels = hypixelPlayer?.stats?.Duels;
+
+			const duelsWins = duels?.[`wins`] ? (duels[`wins`] as number) : 0;
+			const duelsLosses = duels?.[`losses`] ? (duels[`losses`] as number) : 0;
+			const duelsWLR = formatRatio(duelsWins, duelsLosses);
+
+			const playerEmbed = new EmbedBuilder().setTimestamp(Date.now()).setTitle(hypixelPlayer.displayname);
+
+			playerEmbed.addFields(
+				{
+					name: "Bedwars",
+					value: `Level: ${hypixelPlayer?.achievements?.bedwars_level ?? 0}\nWins: ${bedwars?.wins_bedwars ?? 0}`,
+				},
+				{
+					name: "Duels",
+					value: `Wins: ${duelsWins}\nWLR: ${duelsWLR}`,
+				},
+			);
+
+			const requirements = config.guild.requirements;
+			if (requirements.bedwars_wins < (hypixelPlayer?.stats?.Bedwars?.wins_bedwars ?? 0) || requirements.duels_wins < duelsWins) {
+				playerEmbed.setColor("Green");
+			} else {
+				playerEmbed.setColor("Red");
+			}
+
+			await interaction.editReply({ embeds: [playerEmbed] });
+		} else {
+			await interaction.editReply("This name is either invalid or doesn't exist on Hypixel, Try again in a minute or use their UUID.");
 		}
 	}
 };
